98de5d34c6847c0d3931849dd9d5d6eed11020a7
MiguelNegron <miguel.negron@upr.edu>
2018-03-27 21:14:40 -0400

    message handler en progreso

--- handler/messageHandler.py
+++ handler/messageHandler.py
@@ -12,8 +12,8 @@ class messageHandler:
         result['num_likes'] = row[3]
         result['num_dislikes'] = row[4]
         result['num_replies'] = row[5]
-        result['uid'] = row[6]
-        result['gid'] = row[7]
+        result['ownerid'] = row[6]
+        result['groupchatid'] = row[7]
         return result
 
     def getAllMessages(self):
@@ -30,18 +30,50 @@ class messageHandler:
         result['sname'] = row[1]
         return result
 
-    def getPartById(self, id):
+    def getMessageByID(self, mid):
         dao = MessagesDAO()
-        result = dao.getPartById(id)
-        if result == None:
-            return jsonify(Error="NOT FOUND"), 404
-        else :
-            mapped = self.mapToDict(result)
-            return jsonify(Part=mapped)
+        result = dao.getMessageByID(mid)
+        mapped_result = []
+        for r in result:
+            mapped_result.append(self.mapToSupDict(r))
+        return jsonify(Messages=mapped_result)
 
-    def getMessagesByPartId(self, id):
+
+      #  dao = MessagesDAO()
+      #  result = dao.getMessageByID(id)
+      #  if result is None:
+      #      return jsonify(Error="NOT FOUND"), 404
+      #  else:
+      #      mapped = self.mapToDict(result)
+      #      return jsonify(Messages=mapped)
+
+    def getMessagesByUserID(self, uid):
         dao = MessagesDAO()
-        result = dao.getMessagesByPartId(id)
+        result = dao.getMessagesByUserID(uid)
+        mapped_result = []
+        for r in result:
+            mapped_result.append(self.mapToSupDict(r))
+        return jsonify(Messages=mapped_result)
+
+    def getMessagesByGroupChatID(self, gid):
+        dao = MessagesDAO()
+        result = dao.getMessageByGroupChatID(gid)
+        mapped_result = []
+        for r in result:
+            mapped_result.append(self.mapToSupDict(r))
+        return jsonify(Messages=mapped_result)
+
+    def getMessageByText(self, text):
+        dao = MessagesDAO()
+        result = dao.getMessageByText(text)
+        mapped_result = []
+        for r in result:
+            mapped_result.append(self.mapToSupDict(r))
+        return jsonify(Messages=mapped_result)
+
+    def getMessageByDate(self, date):
+        dao = MessagesDAO()
+        result = dao.getMessageByDate(date)
         mapped_result = []
         for r in result:
             mapped_result.append(self.mapToSupDict(r))
@@ -56,12 +88,3 @@ class messageHandler:
             mapped_result.append(self.mapToDict(r))
         return jsonify(Messages=mapped_result)
 
-
-    def searchParts(self, args):
-        color = args.get('color')
-        dao = MessagesDAO()
-        result = dao.searchByColor(color)
-        mapped_result = []
-        for r in result:
-            mapped_result.append(self.mapToDict(r))
-        return jsonify(Part=mapped_result)
