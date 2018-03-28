3b87a083a637a9eb894015d4d2234d9a74d8a132
Rogelio Diaz Vera <rogelio.diaz@upr.edu>
2018-03-27 21:25:12 -0400

    Creation of Reaction Handler

---
+++ handler/reactionHandler.py
@@ -0,0 +1,30 @@
+from flask import jsonify
+from dao.reactionDAO import ReactionDAO
+
+
+class ReactionHandler:
+
+    def mapToDict(self, r):
+        result = {}
+        result['userid'] = r[0]
+        result['messageid'] = [1]
+        result['type'] = r[2]
+        result['reactionid'] = r[3]
+        return result
+
+    def getAllReactions(self):
+        dao = ReactionDAO()
+        result = dao.getAllReaction()
+        mapped_result = []
+        for r in result:
+            mapped_result.append(self.mapToDict(r))
+        return jsonify(Reaction=mapped_result)
+
+    def getReactionById(self, rid):
+        dao = ReactionDAO()
+        result = dao.getReactionById(rid)
+        if result is None:
+            return jsonify(Error="NOT FOUND"), 404
+        else :
+            mapped = self.mapToDict(result)
+            return jsonify(Reaction=mapped)
