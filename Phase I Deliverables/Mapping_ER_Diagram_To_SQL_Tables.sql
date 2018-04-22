05f0c1fdd3f4bfc07f710958f719ac1c5299ea22
Rogelio Diaz Vera <rogelio.diaz@upr.edu>
2018-04-22 14:34:38 -0700

    Update Mapping_ER_Diagram_To_SQL_Tables.sql

--- Phase I Deliverables/Mapping_ER_Diagram_To_SQL_Tables.sql
+++ Phase I Deliverables/Mapping_ER_Diagram_To_SQL_Tables.sql
@@ -5,7 +5,7 @@ create table user (
 	password varchar(25),
 	phone varchar(10), 
 	email varchar(50)
-	)
+	);
 
 create table message (
 	mid serial primary key, 
@@ -13,43 +13,43 @@ create table message (
 	date_created timestamp, 
 	uid integer references user(uid),
 	gid integer references groupchat(gid)
-	)
+	);
 
 create table groupchat (
 	gid serial primary key, 
 	gname varchar(20), 
 	uid integer references user(uid)
-	)
+	);
 
 create table hashtag (
 	hid serial primary key, 
 	hstring varchar(30) unique, 
 	mid integer references message(mid)
-	)
+	);
 
 create table members (
 	uid integer references user(uid),
 	gid integer references groupchat(gid),
 	primary key (uid, gid)
-	)
+	);
 
 create table reactions (
 	uid integer references user(uid),
 	mid integer references message(mid),
 	type varchar(10), 
 	primary key (uid, mid)
-	)
+	);
 
 create table replies (
 	reply_mid integer references message(mid),
 	replied_mid integer references message(mid),
 	primary key (reply_mid, replied_mid)
-	)
+	);
 	
 create table contacts (
 	user1_uid integer references user(uid),
 	user2_uid integer references user(uid),
 	primary key (user1_uid, user2_uid)
-	)
+	);
 	
 
