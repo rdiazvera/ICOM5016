c1b9aa0c38424ce01dfeafd5f277ebaa6194b169
Rogelio Diaz Vera <rogelio.diaz@upr.edu>
2018-04-23 03:01:31 -0400

    Edit Messages Handler.

--- handler/messageHandler.py
+++ handler/messageHandler.py
@@ -1,78 +1,94 @@
 from flask import jsonify, request
 from dao.messageDAO import MessagesDAO
+from handler import buildDict
 
 
-class messageHandler:
+# Handler Class to handle the Messages, Replies, Reactions and Hashtags entities
+class MessageHandler:
 
-    def mapToDict(self, row):
-        result = {}
-        result['mid'] = row[0]
-        result['text'] = row[1]
-        result['date_created'] = row[2]
-        result['num_likes'] = row[3]
-        result['num_dislikes'] = row[4]
-        result['num_replies'] = row[5]
-        result['ownerid'] = row[6]
-        result['groupchatid'] = row[7]
-        return result
+    # === Messages Getters === #
 
+    # List of all messages in the system
     def getAllMessages(self):
         dao = MessagesDAO()
         result = dao.getAllMessages()
         mapped_result = []
         for r in result:
-            mapped_result.append(self.mapToDict(r))
+            mapped_result.append(buildDict.build_messages_dict(r))
         return jsonify(Message=mapped_result)
 
-    def getMessageByID(self, mid):
+    def getMessageById(self, mid):
         dao = MessagesDAO()
-        result = dao.getMessageByID(mid)
+        result = dao.getMessageById(mid)
         mapped = []
         if result is None:
             return jsonify(Error="NOT FOUND"), 404
         else:
             for r in result:
-                mapped = self.mapToDict(r)
+                mapped = buildDict.build_messages_dict(r)
             return jsonify(Message=mapped)
 
-    def getMessagesByOwnerID(self, ownerid):
+    # List of messages posted to a chat group
+    def getMessagesByGroupChatId(self, gid):
         dao = MessagesDAO()
-        result = dao.getMessagesByOwnerID(ownerid)
+        result = dao.getMessageByGroupChatId(gid)
         mapped_result = []
         for r in result:
-            mapped_result.append(self.mapToDict(r))
+            mapped_result.append(buildDict.build_messages_dict(r))
         return jsonify(Message=mapped_result)
 
-    def getMessagesByGroupChatID(self, groupchatid):
+    # === Reactions Getters === #
+
+    def getAllReactions(self):
         dao = MessagesDAO()
-        result = dao.getMessageByGroupChatID(groupchatid)
+        result = dao.getAllReactions()
         mapped_result = []
         for r in result:
-            mapped_result.append(self.mapToDict(r))
-        return jsonify(Message=mapped_result)
+            mapped_result.append(buildDict.build_reactions_dict(r))
+        return jsonify(Reaction=mapped_result)
 
-    def getMessageByText(self, text):
+    # Number of likes to a message
+    def getNumberOfLikes(self, mid):
         dao = MessagesDAO()
-        result = dao.getMessageByText(text)
+        result = dao.getMessageById(mid)
+        mapped = []
+        for r in result:
+            # TODO: Edit
+            mapped = buildDict
+        return jsonify(Message=mapped)
+
+    # List of users who liked a message
+    def getUsersWhoLikeMessage(self, mid):
+        dao = MessagesDAO()
+        result = dao.getMessageByGroupChatId(mid)
         mapped_result = []
         for r in result:
-            mapped_result.append(self.mapToDict(r))
+            # TODO: Edit
+            mapped_result.append(buildDict.build_users_dict(r))
         return jsonify(Message=mapped_result)
 
-    def getMessageByDate(self, date):
+    # Number of dislikes to a message
+    def getNumberOfDislikes(self, mid):
         dao = MessagesDAO()
-        result = dao.getMessageByDate(date)
+        result = dao.getMessageById(mid)
+        mapped = []
+        for r in result:
+            # TODO: Edit
+             mapped = buildDict
+        return jsonify(Message=mapped)
+
+
+    # List of users who dislikes a message
+    def getUsersWhoDislikeMessage(self, mid):
+        dao = MessagesDAO()
+        result = dao.getMessageByGroupChatId(mid)
         mapped_result = []
         for r in result:
-            mapped_result.append(self.mapToDict(r))
+            # TODO: Edit
+            mapped_result.append(buildDict.build_users_dict(r))
         return jsonify(Message=mapped_result)
 
-    def getMessageBy(self, args):
-        ownerID = args.get('id')
-        dao = MessagesDAO()
-        result = dao.getMessagesByOwnerID(ownerID)
-        mapped_result = []
-        for r in result:
-            mapped_result.append(self.mapToDict(r))
-        return jsonify(Message=mapped_result)
 
+    # === Replies Getters === #
+
+    # === Hashtags Getters === #
