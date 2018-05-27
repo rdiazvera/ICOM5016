73daf9b36fecbc4bb68c3fb5f2722f5183835a58
MiguelNegron <miguel.negron@upr.edu>
2018-05-27 17:29:32 -0400

    register completado

--- appjs/login.js
+++ appjs/login.js
@@ -9,6 +9,12 @@ angular.module('AppChat').controller('LoginController', ['$http', '$log', '$scop
    var thisCtrl = this;
    this.loginUsername = "";
    this.loginPassword = "";
+   this.registerEmail = "";
+   this.registerPassword = "";
+   this.registerPhone = "";
+   this.registerFirstName = "";
+   this.registerLastName = "";
+   this.registerUsername = "";
 
             this.LogIn = function(){
 //            var msg = thisCtrl.newText;
@@ -38,6 +44,40 @@ angular.module('AppChat').controller('LoginController', ['$http', '$log', '$scop
                 function(response){
                     if (status == 0){
                         alert("Invalid username or password");
+                    }
+                    else if (status == 401){
+                        alert("Session Expired.");
+                    }
+                    else if (status == 403){
+                        alert("Not authorized.");
+                    }
+                    else if (status == 404){
+                        alert("Not Found.");
+                    }
+                    else {
+                        alert("Internal system error.");
+                    }
+                });
+
+        };
+
+            this.Register = function(){
+
+            var url = "http://127.0.0.1:5000/MessagingApp_DB/users/register/";
+            var data = JSON.stringify({username: thisCtrl.registerUsername, password: thisCtrl.registerPassword,
+            first_name: thisCtrl.registerFirstName, last_name: thisCtrl.registerLastName, email: thisCtrl.registerEmail,
+            phone: thisCtrl.registerPhone});
+
+            $http.post(url, data).then(
+
+                function(response){
+                    alert("User Registered Successfully");
+                    location.reload();
+
+                },
+                function(response){
+                    if (status == 0){
+                        alert("Invalid or empty field(s)");
                     }
                     else if (status == 401){
                         alert("Session Expired.");
